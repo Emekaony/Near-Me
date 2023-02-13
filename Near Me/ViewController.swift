ab7345290a9bf7e40ab5606f8f11ac9e2474d0f6
Nnaemeka <Nnaemekaonyeokoro@outlook.com>
2023-02-12 23:34:31 -0800

    Initial commit

---
+++ Near Me/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Near Me
+//
+//  Created by SHAdON . on 2/12/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .blue
+    }
+
+
+}
+
