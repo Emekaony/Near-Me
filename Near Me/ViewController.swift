4cdb274ce4ad6c9677b785a61987799f2c03e35c
Nnaemeka <Nnaemekaonyeokoro@outlook.com>
2023-02-13 10:16:23 -0800

    Added the search nar component

--- Near Me/ViewController.swift
+++ Near Me/ViewController.swift
@@ -16,6 +16,18 @@ class ViewController: UIViewController {
         map.translatesAutoresizingMaskIntoConstraints = false
         return map
     }()
+    
+    lazy var searchTextField: UITextField = {
+        let searchTextField = UITextField()
+        searchTextField.layer.cornerRadius = 10
+        searchTextField.clipsToBounds = true
+        searchTextField.backgroundColor = .white
+        searchTextField.placeholder = "Search"
+        searchTextField.leftView = UIView(frame: CGRect(x: 0, y: 0, width: 10, height: 0))
+        searchTextField.leftViewMode = .always
+        searchTextField.translatesAutoresizingMaskIntoConstraints = false
+        return searchTextField
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -24,7 +36,20 @@ class ViewController: UIViewController {
     }
     
     private func setupUI() {
+        
+        view.addSubview(searchTextField)
         view.addSubview(mapView)
+        
+        view.bringSubviewToFront(searchTextField)
+        
+        // search constraints
+        searchTextField.heightAnchor.constraint(equalToConstant: 44).isActive = true
+        searchTextField.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        searchTextField.widthAnchor.constraint(equalToConstant: view.bounds.size.width/1.2).isActive = true
+        searchTextField.topAnchor.constraint(equalTo: view.topAnchor, constant: 60).isActive = true
+        searchTextField.returnKeyType = .go
+        
+        
         
         // add constraints to the mapView
         mapView.widthAnchor.constraint(equalTo: view.widthAnchor).isActive = true
